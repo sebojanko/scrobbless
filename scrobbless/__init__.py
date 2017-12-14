521ebfa6f7d8763adda370a0eaebddc841b79832
sebojanko <janko.sebastian@gmail.com>
2017-12-14 21:49:12 +0100

    Initial commit

---
+++ scrobbless/__init__.py
@@ -0,0 +1,1 @@
+from .scrobbless improt Scrobbless
---
+++ scrobbless/scrobbless.py
@@ -0,0 +1,13 @@
+import json
+
+class Scrobbless:
+  def __init__(self):
+
+  def get_token(self):
+    pass
+
+  def get_session(self):
+    pass
+
+  def create_signature(self):
+    pass
