0a01ab1d23b75358447556108f402dcffd43a514
sebojanko <janko.sebastian@gmail.com>
2018-03-03 13:27:47 +0100

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from setuptools import setup
 
 setup(name='scrobbless',
-      version='0.2',
+      version='0.3.0',
       description='Minimal last.fm scrobbler',
       url='http://github.com/sebojanko/scrobbless',
       author='Sebastian Janko',
       author_email='janko.sebastian@gmail.com',
       license='GNU GPL v3',
+      keywords='last.fm scrobbler',
       packages=['scrobbless'],
       zip_safe=False,
       install_requires=['requests'])
